7cafcefd977d58e8655ec480f34a96bc9461c950
Tim Bright <capoeirista.muralha@gmail.com>
2016-03-24 22:20:52 -0700

    Added fix for GEMV function.

--- gemv.js
+++ gemv.js
@@ -4,7 +4,7 @@ var blas1 = require('ndarray-blas-level1');
 
 exports.gemv = function (alpha, A, x, beta, y) {
   var dot = blas1.dot;
-  for (var i = A.shape[1] - 1; i >= 0; i--) {
+  for (var i = A.shape[0] - 1; i >= 0; i--) {
     y.set(i, y.get(i) * beta + alpha * dot(A.pick(i, null), x));
   }
   return true;
