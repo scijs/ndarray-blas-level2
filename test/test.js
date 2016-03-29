9849c84250956aca1d8cc8279098ef19b0add121
Tim Bright <capoeirista.muralha@gmail.com>
2016-03-28 21:33:50 -0700

    Converted test.js to support more testing
    Changed tests to leverage the randomized matrix functions to provide better coverage.

--- test/test.js
+++ test/test.js
@@ -6,63 +6,115 @@ var ndarray = require('ndarray');
 var pool = require('ndarray-scratch');
 var ops = require('ndarray-ops');
 var blas2 = require('../index.js');
+var blas1 = require('ndarray-blas-level1');
+var randMatGen = require('./rand-matrix-gen.js');
 
-assert.ndCloseTo = function (a, b, tol) {
+var NUM_TESTS = 2000;
+var TEST_TOLERANCE = 1e-8;
+
+assert.ndCloseTo = function (a, b, tol, msg) {
   assert(a.dimension === b.dimension, 'expected dimension ' + a.dimension + ' to equal dimension ' + b.dimension + '.');
   assert.deepEqual(a.shape, b.shape, 'expected shape ' + a.shape + ' to equal shape ' + b.shape + '.');
   var c = pool.zeros(a.shape, a.dtype);
   ops.sub(c, a, b);
   var err = ops.norm2(c);
-  assert(err < tol, 'Expected error ' + err + ' to be less than tolerance ' + tol + '.');
+  var errorMsg = msg || '';
+  assert(err < tol, 'Expected error ' + err + ' to be less than tolerance ' + tol + '.\n' + errorMsg);
+};
+
+var naiveGEMV = function (alpha, A, x, beta, y) {
+  var m = A.shape[0];
+  var n = A.shape[1];
+  for (var i = 0; i < m; ++i) {
+    var d = 0;
+    for (var j = 0; j < n; ++j) {
+      d += A.get(i,j) * x.get(j);
+    }
+    y.set(i, alpha * d + beta * y.get(i));
+  }
+  return true;
 };
 
 describe('BLAS Level 2', function () {
-  var A;
-  var A0;
-  var x;
-  var x0;
-  var y;
-  // var y0;
-  beforeEach(function () {
-    var Adata = [1, 2, 5, 3];
-    var xdata = [-4, 7];
-    var ydata = [3, -2];
-    A = ndarray(new Float64Array(Adata), [2, 2]);
-    A0 = ndarray(new Float64Array(Adata), [2, 2]);
-    x = ndarray(new Float64Array(xdata));
-    x0 = ndarray(new Float64Array(xdata));
-    y = ndarray(new Float64Array(ydata));
-    // y0 = ndarray(new Float64Array(ydata));
-  });
+  var m = 10;
+  var n = 15;
+  var alpha = 0;
+  var beta = 0;
+  var seed = undefined;
+  var matGen = new randMatGen(seed, Float64Array);
+  var A = ndarray(new Float64Array(m * n), [m, n]);
+  var B = ndarray(new Float64Array(n * n), [n, n])
+  var x = ndarray(new Float64Array(n), [n]);
+  var x0 = ndarray(new Float64Array(n), [n]);
+  var xn = ndarray(new Float64Array(n), [n]);
+  var y = ndarray(new Float64Array(m), [m]);
+  var y0 = ndarray(new Float64Array(m), [m]);
+
+  // beforeEach(function () {
+    
+  // });
 
   it('gemv', function () {
-    assert(blas2.gemv(-4, A, x, 2, y));
-    assert.ndCloseTo(ndarray(new Float64Array([-34, -8])), y, 1e-8);
-    assert.ndCloseTo(A0, A, 1e-8);
-    assert.ndCloseTo(x0, x, 1e-8);
+    for(var t = 0; t < NUM_TESTS; ++t) {
+      alpha = Math.random();
+      beta = Math.random();
+      seed = matGen.setRandomSeed(36);
+      matGen.makeGeneralMatrix(m, n, A);
+      matGen.makeGeneralMatrix(1, n, x);
+
+      assert(blas2.gemv(alpha, A, x, beta, y));
+      assert(naiveGEMV(alpha, A, x, beta, y0));
+      assert.ndCloseTo(y0, y, TEST_TOLERANCE, 'Failure seed value: "' + seed + '".');
+    }
   });
 
   it('trmv', function () {
-    assert(blas2.trmv(A, x));
-    assert.ndCloseTo(x, ndarray([10, 21]), 1e-8);
-    assert.ndCloseTo(A0, A, 1e-8);
+    for(var t = 0; t < NUM_TESTS; ++t) {
+      seed = matGen.setRandomSeed(36);
+      matGen.makeTriangularMatrix(n, n, false, B);
+      matGen.makeGeneralMatrix(1, n, xn);
+      blas1.copy(xn, x);
+
+      assert(blas2.trmv(B, x, false));
+      assert(blas2.gemv(1.0, B, xn, 0.0, x0));
+      assert.ndCloseTo(x, x0, TEST_TOLERANCE, 'Failure seed value: "' + seed + '".');
+    }
   });
 
   it('trmv lower', function () {
-    assert(blas2.trmv(A, x, true));
-    assert.ndCloseTo(x, ndarray([-4, 1]), 1e-8);
-    assert.ndCloseTo(A0, A, 1e-8);
+    for(var t = 0; t < NUM_TESTS; ++t) {
+      seed = matGen.setRandomSeed(36);
+      matGen.makeTriangularMatrix(n, n, true, B);
+      matGen.makeGeneralMatrix(1, n, xn);
+      blas1.copy(xn, x);
+
+      assert(blas2.trmv(B, x, true));
+      assert(blas2.gemv(1.0, B, xn, 0.0, x0));
+      assert.ndCloseTo(x, x0, TEST_TOLERANCE, 'Failure seed value: "' + seed + '".');
+    }
   });
 
   it('trsv', function () {
-    assert(blas2.trsv(A, x));
-    assert.ndCloseTo(x, ndarray([-8.66666666666666667, 2.3333333333333333]), 1e-8);
-    assert.ndCloseTo(A0, A, 1e-8);
+    for(var t = 0; t < NUM_TESTS; ++t) {
+      seed = matGen.setRandomSeed(36);
+      matGen.makeTriangularMatrix(n, n, false, B);
+      matGen.makeGeneralMatrix(1, n, x);
+      
+      assert(blas2.gemv(1.0, B, x, 0.0, x0));
+      assert(blas2.trsv(B, x0, false)); // value of x
+      assert.ndCloseTo(x, x0, TEST_TOLERANCE, 'Failure seed value: "' + seed + '".');
+    }
   });
 
   it('trsv lower', function () {
-    assert(blas2.trsv(A, x, true));
-    assert.ndCloseTo(x, ndarray([-4, 9]), 1e-8);
-    assert.ndCloseTo(A0, A, 1e-8);
+    for(var t = 0; t < NUM_TESTS; ++t) {
+      seed = matGen.setRandomSeed(36);
+      matGen.makeTriangularMatrix(n, n, true, B);
+      matGen.makeGeneralMatrix(1, n, x);
+      
+      assert(blas2.gemv(1.0, B, x, 0.0, x0));
+      assert(blas2.trsv(B, x0, true)); // value of x
+      assert.ndCloseTo(x, x0, TEST_TOLERANCE, 'Failure seed value: "' + seed + '".');
+    }
   });
 });
